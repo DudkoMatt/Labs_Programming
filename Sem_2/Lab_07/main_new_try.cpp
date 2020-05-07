0738786b76725c30ba965eaddafe6382ca56e5fc
DudkoMatt <github.dudko.matt@mail.ru>
2020-05-07 03:03:41 +0300

    Lab_07: fixed tail in erase

--- Sem_2/Lab_07/main_new_try.cpp
+++ Sem_2/Lab_07/main_new_try.cpp
@@ -308,7 +308,10 @@ public:
             ++it;
         }
 
-        tail = (tail - 1 + capacity) % capacity;
+        if (tail == capacity)
+            tail = (head + size - 1) % capacity;
+        else
+            tail = (tail - 1 + capacity) % capacity;
         size--;
     }
 
@@ -370,15 +373,15 @@ int main() {
 //    buffer.push_back(0);
     buffer.push_back(1);
     buffer.push_back(2);
-//    buffer.push_back(3);
+    buffer.push_back(3);
 //    buffer.pop_back();
 //    buffer.push_back(3);
+    buffer.pop_front();
 //    buffer.pop_front();
 //    buffer.pop_front();
 //    buffer.pop_front();
-//    buffer.pop_front();
-//    buffer.push_back(4);
-//    buffer.push_back(5);
+    buffer.push_back(4);
+    buffer.push_back(5);
 
 
     // ToDO: try insert
